d0d62e3b7ef8e1ac690bd12a656a64606e1b9a56
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-23 20:45:29 +0500

    Initial Commit

---
+++ Slider SwiftUI/Slider_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slider_SwiftUIApp.swift
+//  Slider SwiftUI
+//
+//  Created by iMac on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Slider_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
