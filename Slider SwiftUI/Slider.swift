09a8a4ed12056550d7e7dff25968918a01287814
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-23 20:52:07 +0500

    SliderView created.

---
+++ Slider SwiftUI/Slider.swift
@@ -0,0 +1,20 @@
+//
+//  Slider.swift
+//  Slider SwiftUI
+//
+//  Created by iMac on 23.11.2021.
+//
+
+import SwiftUI
+
+struct Slider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Slider_Previews: PreviewProvider {
+    static var previews: some View {
+        Slider()
+    }
+}
