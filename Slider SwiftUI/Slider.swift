20536d23ff87c6ef5e94435af91a308ead572e76
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-23 21:14:34 +0500

    SliderView done.

--- Slider SwiftUI/Slider.swift
+++ Slider SwiftUI/Slider.swift
@@ -7,14 +7,54 @@
 
 import SwiftUI
 
-struct Slider: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Slider: UIViewRepresentable {
+    
+    @Binding var value: Double //значение ползунка берем из ContentView
+    let alpha: Int
+    let color: UIColor
+    
+    //создаем слайдер
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueChanged),
+            for: .valueChanged
+        )
+        return slider
+    }
+    
+    func updateUIView(_ view: UISlider, context: Context) {
+        view.thumbTintColor = color.withAlphaComponent(CGFloat(alpha) / 100)
+        view.value = Float(value)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $value)
+    }
+}
+
+//класс Coordinator
+extension Slider {
+    class Coordinator: NSObject {
+        
+        @Binding var value: Double
+        
+        init(value: Binding<Double>) {
+            self._value = value
+        }
+        
+        @objc func valueChanged(_ sender: UISlider) {
+            value = Double(sender.value)
+        }
     }
 }
 
 struct Slider_Previews: PreviewProvider {
     static var previews: some View {
-        Slider()
+        Slider(value: .constant(50), alpha: 100, color: .red)
     }
 }
